835563afdf3feae6a3a084b715a1b58b4f5434ac
Foysal Mahmud <foysalmahmudanik000@gmail.com>
2026-04-21 20:17:12 +0600

    Update chat.js

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -7,7 +7,7 @@ exports.handler = async (event) => {
     const API_KEY = process.env.GEMINI_API_KEY; 
     
     
-    const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+    const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${API_KEY}`;
 
     const response = await fetch(API_URL, {
       method: 'POST',
